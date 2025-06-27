f4bddfd081a5c03ad9d869602d4ce3ae4f70f369
Lewis-Murimi <lewismurimi195@gmail.com>
2025-06-27 19:58:11 +0300

    table description

---
+++ task_4.sql
@@ -0,0 +1,16 @@
+-- USE THE DATABASE
+USE alx_book_store;
+
+-- GET Books DESCRIPTION
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books';
